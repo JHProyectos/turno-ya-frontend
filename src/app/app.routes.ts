b2d1363b3e64d3e506e882dd361c0819a3390ba4
Lucuano123 <luliquaranta6@gmail.com>
2025-11-08 09:12:30 -0300

    Creación rama HU03-reservar-turno

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { Bookings } from './bookings/bookings';
-import { Customers } from '../../../obsoletos/customers';
+//import { Customers } from '../../../obsoletos/customers';
 import { CustomerList } from './customers/pages/customer-list/customer-list';
 import { CustomerForm } from './customers/pages/customer-form/customer-form';
 import { CustomerDetail } from './customers/pages/customer-detail/customer-detail';
+import { BookingList } from './bookings/pages/booking-list/booking-list';
+import { BookingForm } from './bookings/pages/booking-form/booking-form';
+import { BookingDetail } from './bookings/pages/booking-detail/booking-detail';
 import { Services } from './services/services';
 import { Home } from './home/home';
 import { Component } from '@angular/core';
@@ -13,9 +16,9 @@ export const routes: Routes = [
   { path: 'bookings', component: Bookings },
   { path: 'customers', component: CustomerList },
   { path: 'services', component: Services },
- // { path: 'booking/new', component: BookingForm },
- // { path: 'booking/:id', component: BookingDetail },
- // { path : 'booking/:id/edit', component: BookingForm },
+  { path: 'booking/new', component: BookingForm },
+  { path: 'booking/:id', component: BookingDetail },
+  { path : 'booking/:id/edit', component: BookingForm },
   { path: 'customer/new', component: CustomerForm },
   { path: 'customer/:id', component: CustomerDetail },
   { path : 'customer/:id/edit', component: CustomerForm },
