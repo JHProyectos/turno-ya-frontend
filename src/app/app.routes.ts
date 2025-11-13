d5d935e883be57d2fa66b6f1674cd9933f6e8db6
Lucuano123 <luliquaranta6@gmail.com>
2025-11-13 17:08:40 -0300

    booking-list funciona

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
-import { Bookings } from './bookings/bookings';
-//import { Customers } from '../../../obsoletos/customers';
 import { CustomerList } from './customers/pages/customer-list/customer-list';
 import { CustomerForm } from './customers/pages/customer-form/customer-form';
 import { CustomerDetail } from './customers/pages/customer-detail/customer-detail';
@@ -13,7 +11,7 @@ import { Component } from '@angular/core';
 
 export const routes: Routes = [
   { path: '', component: Home },  
-  { path: 'bookings', component: Bookings },
+  { path: 'bookings', component: BookingList },
   { path: 'customers', component: CustomerList },
   { path: 'services', component: Services },
   { path: 'booking/new', component: BookingForm },
--- src/app/shared/materialModule.ts
+++ src/app/shared/materialModule.ts
@@ -5,6 +5,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+
 
 
 export const MaterialModule = [
@@ -14,5 +19,9 @@ export const MaterialModule = [
   MatIconModule,
   MatButtonModule,
   MatProgressSpinnerModule,
-  MatTableModule
+  MatTableModule,
+  FormsModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatOptionModule
 ] as const;
